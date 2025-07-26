e17c49a2a12fc5e9f205e46cff97427830674c83
ndoumbe22 <diopnoumse@gmail.com>
2025-07-26 15:07:24 +0200

    formulaire d'inscription et de connexion que j'ai changé

--- restaurant_site/forms.py
+++ restaurant_site/forms.py
@@ -3,26 +3,35 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, Pass
 from .models import Utilisateur
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label="Nom d'utilisateur")
-    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
+    username = forms.CharField(
+        label="Nom d'utilisateur",
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    password = forms.CharField(
+        label="Mot de passe",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
 
 class RegisterForm(UserCreationForm):
-    role = forms.ChoiceField(choices=Utilisateur.ROLE_CHOICES)
+    role = forms.ChoiceField(
+        choices=Utilisateur.ROLE_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
 
-    class Meta(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
         model = Utilisateur
         fields = UserCreationForm.Meta.fields + ('role',)
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
         self.fields['role'].initial = Utilisateur.CLIENT
 
 class CustomPasswordResetForm(PasswordResetForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-
-
-
-
-
-
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
